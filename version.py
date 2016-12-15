8b4fd7e83744f0309b94323b1537b8f32b9eba4c
John Vivian <jtvivian@gmail.com>
2016-12-15 14:54:21 -0800

    Bump toil-lib version

--- version.py
+++ version.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version = '3.0.1a1'
+version = '3.0.0a1'
 
-required_versions = {'toil-lib': '==1.1.0',
+required_versions = {'toil': '==3.3.1',
+                     'toil-lib': '==1.1.1',
                      'pyyaml': '==3.11'}
