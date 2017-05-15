d87d28351d45b2c9b1baf0eee83a58249c12e228
John Vivian <jtvivian@gmail.com>
2017-05-15 10:39:40 -0700

    Bump to next dev version

--- version.py
+++ version.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version = '3.3.0'
+version = '3.3.1a1'
 
 required_versions = {'toil': '>=3.7.0',
                      'toil-lib': '==1.1.8',
