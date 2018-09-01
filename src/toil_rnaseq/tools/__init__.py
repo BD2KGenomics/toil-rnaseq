6aa87740c44fdb9186dc03b61dcd4ce00e499c80
John Vivian <jtvivian@gmail.com>
2018-09-01 14:48:38 -0700

    Update version of rsemgenemapping to support zero mapped genes (resolves #154)

--- src/toil_rnaseq/tools/__init__.py
+++ src/toil_rnaseq/tools/__init__.py
@@ -2,7 +2,7 @@ cutadapt_version = 'quay.io/ucsc_cgl/cutadapt:1.9--6bd44edd2b8f8f17e25c5a268feda
 fastqc_version = 'quay.io/ucsc_cgl/fastqc:0.11.5--be13567d00cd4c586edf8ae47d991815c8c72a49'
 star_version = 'quay.io/ucsc_cgl/star:2.4.2a--bcbd5122b69ff6ac4ef61958e47bde94001cfe80'
 rsem_version = 'quay.io/ucsc_cgl/rsem:1.2.25--d4275175cc8df36967db460b06337a14f40d2f21'
-rsemgenemapping_version = 'quay.io/ucsc_cgl/gencode_hugo_mapping:1.0--cb4865d02f9199462e66410f515c4dabbd061e4d'
+rsemgenemapping_version = 'quay.io/ucsc_cgl/gencode_hugo_mapping:1.0--fbc4fd7d1d8f6dff556dd7668854b8cb1be60799'
 kallisto_version = 'quay.io/ucsc_cgl/kallisto:0.43.1--355c19b1fb6fbb85f7f8293e95fb8a1e9d0da163'
 hera_version = 'quay.io/ucsc_cgl/hera:1.1--5f2b2b45776536d46a60b377b52b846694c534bd'
 
