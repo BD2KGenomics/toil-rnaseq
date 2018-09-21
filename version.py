0ac2cbb69b694cc63c5650938bec258d56cba5b2
John Vivian <jtvivian@gmail.com>
2018-09-20 23:28:26 -0700

    Release 4.0.4

--- version.py
+++ version.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version = '4.0.3'
+version = '4.0.4'
 
 required_versions = {'toil': '>=3.12.0',
                      'pyyaml': '>=3.11'}
